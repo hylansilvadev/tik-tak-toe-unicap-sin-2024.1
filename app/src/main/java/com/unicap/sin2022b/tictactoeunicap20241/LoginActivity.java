960168d34c9444eeae6d4e85b69dbc90cbb3d1e3
Mariana Marinho <mariana.00000847857@unicap.br>
2024-06-03 02:17:11 -0300

    Adicionando o firebase Auth, junto ao Firebase Storage, e criando o login na plataforma

---
+++ app/src/main/java/com/unicap/sin2022b/tictactoeunicap20241/LoginActivity.java
@@ -0,0 +1,115 @@
+package com.unicap.sin2022b.tictactoeunicap20241;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btn_singin;
+    FirebaseAuth auth;
+    FirebaseFirestore database;
+
+    GoogleSignInClient googleSignInClient;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btn_singin = findViewById(R.id.BtnSingin);
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseFirestore.getInstance();
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(com.firebase.ui.auth.R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+        btn_singin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                btn_singin();
+            }
+        });
+    }
+
+    int RC_SIGN_IN = 40;
+
+    private void btn_singin() {
+        Intent intent = googleSignInClient.getSignInIntent();
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuth(account.getIdToken());
+            } catch (ApiException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+    private void firebaseAuth(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if( task.isSuccessful() ){
+                    FirebaseUser user = auth.getCurrentUser();
+                    Users currentUser = new Users();
+
+                    assert user != null;
+                    currentUser.setUserId(user.getProviderId());
+                    currentUser.setName(user.getDisplayName());
+                    currentUser.setProfile(Objects.requireNonNull(user.getPhotoUrl()).toString());
+
+                    database.collection("users").document(user.getUid()).set(currentUser);
+
+                    Intent intent = new Intent(LoginActivity.this, DetailsActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "Login Falhou", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/unicap/sin2022b/tictactoeunicap20241/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.unicap.sin2022b.tictactoeunicap20241;
-
-import android.app.Activity;
-
-public class MainActivity extends Activity {
-
-}
